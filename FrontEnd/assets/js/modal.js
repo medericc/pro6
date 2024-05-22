f6bd48dd31245283155337fc7703b5ac02e8041f
Mede <dalemale09@gmail.com>
2024-05-22 12:57:28 +0200

    field almost good but output no display

--- FrontEnd/assets/js/modal.js
+++ FrontEnd/assets/js/modal.js
@@ -3,18 +3,15 @@ document.addEventListener("DOMContentLoaded", () => {
     const modalAjout = document.getElementById('modalAjoutPhoto');
     const closeButton = document.querySelectorAll('.close');
     const ferme = document.getElementById('ferme');
-    let modalShouldClose = true;
-
-    // Fermer la modale
-    ferme.addEventListener('click', closeModal);
-
-    // Formulaire ajout photo
     const submitButton = document.getElementById('butAdd');
     const fileInput = document.getElementById('pictureProjet');
     const titleInput = document.getElementById('titrePhoto');
     const categoryInput = document.getElementById('categoriePhoto');
     const output = document.getElementById('output');
     const field = document.querySelector('.field');
+    let modalShouldClose = true;
+
+    ferme.addEventListener('click', closeModal);
 
     function checkFormCompletion() {
         if (fileInput.files.length > 0 && titleInput.value.trim() !== "" && categoryInput.value.trim() !== "") {
@@ -32,7 +29,7 @@ document.addEventListener("DOMContentLoaded", () => {
             output.src = URL.createObjectURL(file);
             output.onload = function() {
                 URL.revokeObjectURL(output.src);
-            }
+            };
             field.classList.add('visible');
         } else {
             field.classList.remove('visible');
@@ -40,18 +37,17 @@ document.addEventListener("DOMContentLoaded", () => {
         checkFormCompletion();
     });
 
-    titleInput.addEventListener('change', checkFormCompletion);
-    categoryInput.addEventListener('change', checkFormCompletion);
+    titleInput.addEventListener('input', checkFormCompletion);
+    categoryInput.addEventListener('input', checkFormCompletion);
 
-   
     const modifier = document.getElementById('modifierProjetLink');
     modifier.addEventListener('click', () => {
         aside.classList.remove("dnone");
     });
 
     function openModal(e) {
-        document.getElementById('overlay').style.display = 'block';
         e.preventDefault();
+        document.getElementById('overlay').style.display = 'block';
 
         const target = document.querySelector(e.target.getAttribute('href'));
         if (!target) {
@@ -86,7 +82,7 @@ document.addEventListener("DOMContentLoaded", () => {
             const response = await fetch('http://localhost:5678/api/works');
             const data = await response.json();
             await populateModalWithData(data);
-            await populateMainGallery(data);  
+            await populateMainGallery(data);
             return data;
         } catch (error) {
             console.error('Erreur de récupération des données des travaux:', error);
@@ -97,13 +93,13 @@ document.addEventListener("DOMContentLoaded", () => {
     async function populateModalWithData(data) {
         const modaleSectionProjets = document.querySelector('#gallery-modal');
         modaleSectionProjets.innerHTML = '';
-        for (let i = 0; i < data.length; i++) {
+        data.forEach(item => {
             const div = document.createElement("div");
             modaleSectionProjets.appendChild(div);
 
             const img = document.createElement("img");
-            img.src = data[i].imageUrl;
-            img.alt = data[i].title;
+            img.src = item.imageUrl;
+            img.alt = item.title;
             div.appendChild(img);
             div.classList.add("imageGallery");
 
@@ -118,7 +114,7 @@ document.addEventListener("DOMContentLoaded", () => {
                     if (!token) {
                         throw new Error("L'utilisateur n'est pas authentifié.");
                     }
-                    const response = await fetch(`http://localhost:5678/api/works/${data[i].id}`, {
+                    const response = await fetch(`http://localhost:5678/api/works/${item.id}`, {
                         method: "DELETE",
                         headers: {
                             'Authorization': `Bearer ${token}`
@@ -128,7 +124,7 @@ document.addEventListener("DOMContentLoaded", () => {
                         throw new Error("La suppression du travail a échoué.");
                     }
                     div.remove();
-                    document.getElementById('figure' + data[i].id).remove();
+                    document.getElementById('figure' + item.id).remove();
                 } catch (error) {
                     console.error("Erreur lors de la suppression du travail :", error);
                 } finally {
@@ -136,55 +132,45 @@ document.addEventListener("DOMContentLoaded", () => {
                 }
             });
             div.appendChild(deleteButton);
-        }
+        });
     }
 
     async function populateMainGallery(data) {
         const galerie = document.getElementById('gallery');
         galerie.innerHTML = '';
-        for (let i = 0; i < data.length; i++) {
+        data.forEach(item => {
             const figure = document.createElement('figure');
-            figure.id = 'figure' + data[i].id;
+            figure.id = 'figure' + item.id;
             const image = document.createElement('img');
-            image.src = data[i].imageUrl;
-            image.alt = data[i].title;
+            image.src = item.imageUrl;
+            image.alt = item.title;
             figure.appendChild(image);
             const figcaption = document.createElement('figcaption');
-            figcaption.textContent = data[i].title;
+            figcaption.textContent = item.title;
             figure.appendChild(figcaption);
             galerie.appendChild(figure);
-        }
+        });
     }
 
     async function modalAjoutPhoto() {
         aside.classList.add("dnone");
         modalAjout.classList.remove("dnone");
-    
+
         const formAjoutPhoto = document.getElementById('formAjoutPhoto');
         const backButton = modalAjout.querySelector('#retourGalerie');
-        const submitButton = document.getElementById('butAdd');
-        const fileInput = document.getElementById('pictureProjet');
-        const titleInput = document.getElementById('titrePhoto');
-        const categoryInput = document.getElementById('categoriePhoto');
-        const output = document.getElementById('output');
-        const field = document.querySelector('.field');
-    
         backButton.addEventListener('click', function(event) {
             event.preventDefault();
             aside.classList.remove("dnone");
             modalAjout.classList.add("dnone");
         });
-    
+
         formAjoutPhoto.addEventListener('submit', async function(event) {
             event.preventDefault();
             submitButton.disabled = true;
             const formData = new FormData();
-            const titrePhoto = titleInput.value;
-            const catPhoto = categoryInput.value;
-            const imgPhoto = fileInput.files[0];
-            formData.append('image', imgPhoto);
-            formData.append('title', titrePhoto);
-            formData.append('category', catPhoto);
+            formData.append('image', fileInput.files[0]);
+            formData.append('title', titleInput.value);
+            formData.append('category', categoryInput.value);
             try {
                 const token = sessionStorage.getItem('token');
                 const response = await fetch('http://localhost:5678/api/works', {
@@ -198,14 +184,13 @@ document.addEventListener("DOMContentLoaded", () => {
                     throw new Error("Échec de l'ajout de la photo.");
                 }
                 const data = await response.json();
-                await modalGal(); 
-    
-               
+                await modalGal();
+
                 formAjoutPhoto.reset();
                 output.src = '#';
                 field.classList.remove('visible');
                 checkFormCompletion();
-    
+
                 aside.classList.remove("dnone");
                 modalAjout.classList.add("dnone");
             } catch (error) {
@@ -218,15 +203,5 @@ document.addEventListener("DOMContentLoaded", () => {
         a.addEventListener('click', openModal);
     });
 
-    const loadFile = function(event) {
-        const output = document.getElementById('output');
-        output.src = URL.createObjectURL(event.target.files[0]);
-        output.onload = function() {
-            URL.revokeObjectURL(output.src);
-        };
-    };
-
-    fileInput.addEventListener('change', loadFile);
-
-    modalGal(); 
+    modalGal();
 });
